75d060a20ffd723e4a22542260a02aeb7ee7cc80
Lester Dale <Blanka4321@gmail.com>
2017-11-23 17:25:29 +0000

    Update RemoveDuplicatesFromLinkedList.java

--- RemoveDuplicatesFromLinkedList.java
+++ RemoveDuplicatesFromLinkedList.java
@@ -1,71 +1,101 @@
-public class RemoveDuplicatesLinkedList{
+public class RemoveDuplicatesLinkedList{    
+  
+  public static void main(String[] args){
     
-    public static void main(String args[]){
+    int[] data = new int[] {1,1,1,1,1,2,1,2,2,2,2,2,2,2,2,2,2,3,3,5,5,3,3,3,3,2,2,2,3,3,3,3,3,3};
+    Node first = null;
+    Node last = null;
+    
+    // create Linkedlist
+    
+    for(int i = 0; i < data.length; i++){
+      
+      Node node = new Node(data[i]);
+      
+      if(first == null){
         
-        int[] data = {2,3,4,5,6,7};
-        Node start = null;
-        Node end   = null;
+        first = node;
+        last = node;
         
-        // create linkedList
+      } 
+      
+      last.next = node;
+      last = node;
+      
+    }
+    
+    print(first);
+    print(removeDuplicates(first));
+    
+  }
+  
+  public static Node removeDuplicates(Node head){
+    
+    int remove_value;
+    Node iter = head;
+    Node current = null;
+    Node previous = null;
+    
+    while(iter != null){
+     
+      remove_value = iter.getData();
+      previous = iter;
+      current = iter.next;
+    
+      while(current != null){
         
-        for(int i = 0; i < data.length; i++){
-            
-            Node node = new Node(data[i]);
-            
-            if(start == null){
-                
-                start = end = node;
-                
-            }
-            
-            end.next = node;
-            end = node;
-            
+        if(current.getData() == remove_value){
+          
+           previous.next = current.next;
+           //previous = previous.next;
+           current = current.next;
+           
+        } else {
+          
+           previous = current;
+           current = current.next;
+          
         }
         
-        print(start);
-        
-    }
-    
-    public static void removeDuplicates(Node start){
-        
-        Node tmp = start;
-        Node previous = null;
-        Node current = null;
-        
-        
-    }
-    
-    public static void print(Node start){
-      
-      Node tmp = start;
-      
-      while(tmp != null){
-          
-          System.out.println(tmp.getData());
-          tmp = tmp.next;
-          
       }
-              
+          
+       iter = iter.next;
+        
+    }    
+      
+    return head;
+      
+    } 
+    
+  
+  public static void print(Node start){
+    
+    Node iter = start;
+    
+    while(iter != null){
+      
+      System.out.print(iter.getData());
+      iter = iter.next;
+      
     }
     
-    public static class Node{
-        
-        private int data;
-        private Node next = null;
-        
-        public Node(int datum){
-            
-            data = datum;
-            
-        }
-        
-        public int getData(){
-            
-            return data;
-            
-        }
-        
-        
+    System.out.println();
+    
+  }
+  
+  public static class Node {
+    
+    private int data;  
+    private Node next = null;
+    
+    public Node(int data){
+      this.data = data;
     }
+    
+    public  int getData(){
+      return data;
+    }
+    
+  }
+  
 }
